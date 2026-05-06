abdf6788505ea828d62e14b2070e5c04c4b064d7
ahmetyasinaktrkoseakut-ui <github-ahmetyasinaktrkoseakut@gmail.com>
2026-05-06 20:47:20 +0300

    Fix: Sidebar menu visibility override for Coordinators

--- components/SidebarNavClient.tsx
+++ components/SidebarNavClient.tsx
@@ -70,7 +70,7 @@ export default function SidebarNavClient({ isAdmin, userId, hasAssignment, isCoo
     }`;
   };
 
-  const showFullMenu = isAdmin || hasAssignment;
+  const showFullMenu = isAdmin || hasAssignment || isCoordinator;
 
   return (
     <>
